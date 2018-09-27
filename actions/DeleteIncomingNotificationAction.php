2fe600b34cde0c731a27bffdc996075929507a7a
KevinEsquivel21 <kevinesquivel21@gmail.com>
2018-09-26 23:28:29 -0600

    fix

--- actions/DeleteIncomingNotificationAction.php
+++ actions/DeleteIncomingNotificationAction.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 
 if(isset($id)){
     $business = new NotificationBusiness();
-    if($business->deteleIncomingNotification($id) == 1){
+    if($business->deleteIncomingNotification($id) == 1){
         header("location: ../view/ShowIncomingNotifications.php?action=1&msg=Registro_eliminado_correctamente");
     }else{
         header("location: ../view/ShowIncomingNotifications.php?action=0&msg=Error_al_eliminar_registro");
